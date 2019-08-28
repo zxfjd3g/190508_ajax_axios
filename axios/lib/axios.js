d272a85049ac415670afac0aa4bebc1e4722d666
zxfjd3g <258147149@qq.com>
2019-08-28 17:02:58 +0800

    init

---
+++ axios/lib/axios.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var utils = require('./utils');
+var bind = require('./helpers/bind');
+var Axios = require('./core/Axios');
+var mergeConfig = require('./core/mergeConfig');
+var defaults = require('./defaults');
+
+/**
+ * Create an instance of Axios
+ *
+ * @param {Object} defaultConfig The default config for the instance
+ * @return {Axios} A new instance of Axios
+ */
+function createInstance(defaultConfig) {
+  // 创建Axios的实例
+  var context = new Axios(defaultConfig);
+  // Axios.prototype.request.bind(context)
+  var instance = bind(Axios.prototype.request, context); // axios
+
+  // Copy axios.prototype to instance
+  utils.extend(instance, Axios.prototype, context); // axios有了request()/get()/post()/put()/delete()
+
+  // Copy context to instance
+  utils.extend(instance, context); // axios有了defaults和interceptors属性
+
+  return instance;
+} // axios()
+
+// Create the default instance to be exported
+var axios = createInstance(defaults);
+
+// Expose Axios class to allow class inheritance
+axios.Axios = Axios;
+
+// Factory for creating new instances
+axios.create = function create(instanceConfig) {
+  return createInstance(mergeConfig(axios.defaults, instanceConfig));
+};
+
+// Expose Cancel & CancelToken
+axios.Cancel = require('./cancel/Cancel');
+axios.CancelToken = require('./cancel/CancelToken');
+axios.isCancel = require('./cancel/isCancel');
+
+// Expose all/spread
+axios.all = function all(promises) {
+  return Promise.all(promises);
+};
+axios.spread = require('./helpers/spread');
+
+module.exports = axios;
+
+// Allow use of default import syntax in TypeScript
+module.exports.default = axios;
